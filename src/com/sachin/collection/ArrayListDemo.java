ce8bd8c9a3137847dada3366635dd46e9687cced
sachin <sachinpatil.mech91@gmail.com>
2023-01-23 20:46:14 -0500

    New classes added

---
+++ src/com/sachin/collection/ArrayListDemo.java
@@ -0,0 +1,12 @@
+package com.sachin.collection;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello sachin");
+
+	}
+
+}
--- src/com/sachin/collection/HashMapDemo.java
+++ src/com/sachin/collection/HashMapDemo.java
@@ -1,11 +1,23 @@
 package com.sachin.collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
 
 	public static void main(String[] args) {
 
 
-		System.out.println("Hello Sachin");
+		Map<Integer, String> map= new HashMap<Integer, String>();
+		
+		map.put(101, "Sachin");
+		map.put(102, "patil");
+		map.put(103, "rane");
+		
+		System.out.println(map);
+		
+		
+		
 	}
 
 }
