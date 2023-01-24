bcdc526a380794615614b681d0808e2fa8375a37
sachin <sachinpatil.mech91@gmail.com>
2023-01-23 21:48:29 -0500

    Arraylist class implementation

--- src/com/sachin/collection/ArrayListDemo.java
+++ src/com/sachin/collection/ArrayListDemo.java
@@ -1,11 +1,22 @@
 package com.sachin.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
 
 
-		System.out.println("Hello sachin");
+		List<String> list = new ArrayList<String>();
+		list.add("sachin");
+		list.add("patil");
+		list.add("raje");
+		
+		for(String str : list )
+		{
+			System.out.println(str);
+		}
 
 	}
 
