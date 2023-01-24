5710ebb22ea8beea8b52f9f1b679f615f34ca2c8
sachin <sachinpatil.mech91@gmail.com>
2023-01-23 20:19:01 -0500

    project-3

---
+++ src/com/sachin/collection/HashMapDemo.java
@@ -0,0 +1,11 @@
+package com.sachin.collection;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello Sachin");
+	}
+
+}
---
+++ src/com/sachin/collection/HashSetDemo.java
@@ -0,0 +1,25 @@
+package com.sachin.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		Set<Integer> set = new HashSet<Integer>();
+		
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		
+		//System.out.println(set);
+		
+		for(Integer i: set) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
